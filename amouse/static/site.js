3fdf12ce3dbe220d1bf9002a83a2f6bb52f03a17
Jack Desert <email>
2021-05-01 15:08:40 -0500

    hide on load

--- amouse/static/site.js
+++ amouse/static/site.js
@@ -1,7 +1,8 @@
 var page = new Vue(
     {
         el: '#container',
-        data: {composeVisible: false},
+        data: {composeVisible: false,
+               message: ''},
         methods: {
             compose: function(event) {
                 this.composeVisible = true
@@ -9,7 +10,35 @@ var page = new Vue(
             stopCompose: function(event) {
                 this.composeVisible = false
             },
-            send: function(event) {
+            sendMessage: function(event) {
+                // See http://youmightnotneedjquery.com
+
+                const request = new XMLHttpRequest()
+                const data = {'message': this.message}
+
+                request.open('POST', '/message/send', true)
+                request.setRequestHeader('Content-Type', 'application/json')
+
+
+                request.onload = function() {
+                    if (request.status >= 200 && request.status < 400) {
+                        // Success!
+                        var resp = request.responseText;
+                        console.log('SUCCESS')
+                    } else {
+                        // We reached our target server, but it returned an error
+                        console.log('ERROR')
+
+                    }
+                }
+
+                request.onerror = function() {
+                    // There was a connection error of some sort
+                        console.log('BIGERROR')
+                }
+
+                request.send(JSON.stringify(data))
+
             }
         }
 
@@ -17,3 +46,7 @@ var page = new Vue(
 )
 
 
+// Show things that were hidded during load
+document.querySelectorAll('.hide-on-load').forEach(function(item, index, array){
+    item.classList.remove('hide-on-load')
+})
